e286898872750bae47aecb4805f786bdbc8a5bb2
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2021-01-08 15:58:59 +0100

    help module

---
+++ include/m_help.hpp
@@ -0,0 +1,8 @@
+#ifndef __MAIN_HELP_MODULE_HPP_____
+#define __MAIN_HELP_MODULE_HPP_____
+#include <vector>
+#include <string>
+
+void help_text(const std::vector<std::string>& args);
+
+#endif
---
+++ src/m_help.cpp
@@ -0,0 +1,56 @@
+#include <m_help.hpp>
+
+void help_text(const std::vector<std::string>& args)
+{
+    std::cout << "NAME" << std::endl;
+    std::cout << "\t" << args[0] << " - raspigcd runner program." << std::endl;
+    std::cout << std::endl;
+    std::cout << "SYNOPSIS" << std::endl;
+    std::cout << "\t" << args[0] << " [options]" << std::endl;
+    std::cout << std::endl;
+    std::cout << "DESCRIPTION" << std::endl;
+    std::cout << "\tIt allows for execution of gcode on Raspberry Pi and simulation of such on desktop." << std::endl;
+    std::cout << std::endl;
+    std::cout << "\t-c <configfile>" << std::endl;
+    std::cout << "\t\tprovide configuration file JSON" << std::endl;
+    std::cout << std::endl;
+    std::cout << "\t-C" << std::endl;
+    std::cout << "\t\tdisplay current configuration in JSON format" << std::endl;
+    std::cout << std::endl;
+    std::cout << "\t-f <filename>" << std::endl;
+    std::cout << "\t\tgcode file to execute" << std::endl;
+    std::cout << std::endl;
+    std::cout << "\t-h" << std::endl;
+    std::cout << "\t\thelp screen" << std::endl;
+    std::cout << std::endl;
+    std::cout << "\t--raw" << std::endl;
+    std::cout << "\t\tTreat the file as raw - no additional processing. No machine limits check (speed, acceleration, ...)." << std::endl;
+    std::cout << std::endl;
+    std::cout << "\t--configtest" << std::endl;
+    std::cout << "\t\tEnables the debug mode for testing configuration" << std::endl;
+    std::cout << std::endl;
+    std::cout << "AUTHOR" << std::endl;
+    std::cout << "\tTadeusz Puźniakowski" << std::endl;
+    std::cout << std::endl;
+    std::cout << "REPORTING BUGS" << std::endl;
+    std::cout << "\tOnline at <https://github.com/pantadeusz/raspigcd2>" << std::endl;
+    std::cout << std::endl;
+    std::cout << "COPYRIGHT" << std::endl;
+    std::cout << "\t" << std::endl;
+    std::cout << "\tRaspberry Pi G-CODE interpreter" << std::endl;
+    std::cout << "\t" << std::endl;
+    std::cout << "\tCopyright (C) 2019 Tadeusz Puźniakowski puzniakowski.pl" << std::endl;
+    std::cout << "\t" << std::endl;
+    std::cout << "\tThis program is free software: you can redistribute it and/or modify" << std::endl;
+    std::cout << "\tit under the terms of the GNU Affero General Public License as published by" << std::endl;
+    std::cout << "\tthe Free Software Foundation, either version 3 of the License, or" << std::endl;
+    std::cout << "\t(at your option) any later version." << std::endl;
+    std::cout << "\t" << std::endl;
+    std::cout << "\tThis program is distributed in the hope that it will be useful," << std::endl;
+    std::cout << "\tbut WITHOUT ANY WARRANTY; without even the implied warranty of" << std::endl;
+    std::cout << "\tMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the" << std::endl;
+    std::cout << "\tGNU Affero General Public License for more details." << std::endl;
+    std::cout << "\t" << std::endl;
+    std::cout << "\tYou should have received a copy of the GNU Affero General Public License" << std::endl;
+    std::cout << "\talong with this program.  If not, see <https://www.gnu.org/licenses/>." << std::endl;
+}
